1630cd65ec87cd51d0b1206605fdd9d2b08b6ac2
Ayzd7 <abuyazid172@gmail.com>
2024-01-18 23:48:36 +0700

    test

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
-document.getElementById("btnPersegi").addEventListener("click", function() {
+let luas_persegi = document.getElementById("luaspersegi")
+let rmspersegi = document.getElementById("rmsluaspersegi")
+let keliling_persegi = document.getElementById("kelilingpersegi")
+let rmskeliling = document.getElementById("rmskeliling")
+
+
+document.getElementById("btnPersegi").addEventListener("click", function () {
     document.getElementById("formPersegi").style.display = "block";
     document.getElementById("formPersegiPanjang").style.display = "none";
 });
 
-document.getElementById("btnPersegiPanjang").addEventListener("click", function() {
-    document.getElementById("formPersegi").style.display     = "none";
+document.getElementById("btnPersegiPanjang").addEventListener("click", function () {
+    document.getElementById("formPersegi").style.display = "none";
     document.getElementById("formPersegiPanjang").style.display = "block";
 });
 
@@ -12,43 +18,29 @@ function hitungLuasPersegi() {
     var sisi = document.getElementById("sisi").value;
     var hasil = sisi * sisi;
     document.getElementById("hasilPersegi").innerText = "Luas Persegi: " + hasil;
+    rmspersegi.innerHTML = `L = S x S`
+    luas_persegi.innerHTML = 'L = ' + sisi + ' x ' + sisi
 }
 
 function hitungLuasPersegiPanjang() {
-    var panjang = document.getElementById("panjang").value;
-    var lebar = document.getElementById("lebar").value;
-    var hasil = panjang * lebar;
-    document.getElementById("hasilPersegiPanjang").innerText = "Luas Persegi Panjang: " + hasil;
-}
-
-// Fungsi untuk menghitung keliling persegi
-function hitungKelilingPersegi() {
-    var sisi = document.getElementById("sisi").value;
-    var keliling = 4 * sisi;
-    document.getElementById("hasilPersegi").innerHTML = "Keliling Persegi: " + keliling;
-}
-
-// Fungsi untuk menghitung keliling persegi panjang
-function hitungKelilingPersegiPanjang() {
-    var panjang = document.getElementById("panjang").value;
-    var lebar = document.getElementById("lebar").value;
-    var keliling = 2 * (parseFloat(panjang) + parseFloat(lebar));
-    document.getElementById("hasilPersegiPanjang").innerHTML = "Keliling Persegi Panjang: " + keliling;
+    var sisipersegipanjang = document.getElementById("sisipersegipanjang").value;
+    var hasil = 4 * sisipersegipanjang;
+    document.getElementById("hasilPersegiPanjang").innerText = "Hitung Keliling Persegi : " + hasil;
+    rmskeliling.innerHTML = `K =  4 x S`
+    keliling_persegi.innerHTML = 'K = ' + 4 + ' * ' + sisipersegipanjang
 }
 
 // Tampilkan form untuk menghitung keliling persegi
-document.getElementById("btnPersegi").addEventListener("click", function() {
+document.getElementById("btnPersegi").addEventListener("click", function () {
     document.getElementById("formPersegi").style.display = "block";
     document.getElementById("formPersegiPanjang").style.display = "none";
 });
 
 // Tampilkan form untuk menghitung keliling persegi panjang
-document.getElementById("btnPersegiPanjang").addEventListener("click", function() {
+document.getElementById("btnPersegiPanjang").addEventListener("click", function () {
     document.getElementById("formPersegiPanjang").style.display = "block";
     document.getElementById("formPersegi").style.display = "none";
 });
-
-// script.js
 
 function resetForm(formId) {
     var form = document.getElementById(formId);
